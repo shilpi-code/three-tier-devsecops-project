75f392b913139de7b31b85b45603e99c4f6fd1fa
shilpi <shilpijainwork@gmail.com>
2026-09-06 20:13:08 +0530

    modifying code message

--- app-code/backend/core/views.py
+++ app-code/backend/core/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
 def api_success(request):
-    return JsonResponse({"message": "API is working successfully!"})
+    return JsonResponse({"message": "API is working completely fine!"})
 
 def root_success(request):
     return JsonResponse({"message": "Welcome to the root endpoint!"})

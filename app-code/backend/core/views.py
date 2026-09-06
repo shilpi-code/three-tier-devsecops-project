77c817e6d33d5d6cb259de7fdcb87aef73607aa3
shilpi <shilpijainwork@gmail.com>
2026-09-06 22:10:20 +0530

    changing message

--- app-code/backend/core/views.py
+++ app-code/backend/core/views.py
@@ -4,4 +4,4 @@ def api_success(request):
     return JsonResponse({"message": "API is working successfully!"})
 
 def root_success(request):
-    return JsonResponse({"message": "Welcome to the root/main endpoint!"})
+    return JsonResponse({"message": "Welcome to the root endpoint!"})
